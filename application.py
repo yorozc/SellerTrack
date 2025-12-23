5fbfa33e0414b97bba1b5e55b7bf7a17233d20d8
yorozc <yorozcolop49@gmail.com>
2025-12-22 23:03:08 -0800

    imported necessary stuff

---
+++ application.py
@@ -0,0 +1,5 @@
+from calc.calc_net_prof import calc_net_prof
+from calc.calc_price_sum import calc_price_sum
+from calc.calc_total_sum import calc_netprof_sum
+from domain.item import Item #item object
+

ba8e9511279b1afe45c14cf14f73de1deda7f38c
yorozc <yorozcolop49@gmail.com>
2026-01-08 13:19:04 -0800

    completed get_netprof_sum, initialized various item functions, created switch statement for interface options

--- application.py
+++ application.py
@@ -3,18 +3,33 @@ from calc.calc_price_sum import calc_price_sum
 from calc.calc_total_sum import calc_netprof_sum
 from domain.item import Item #item object
 
+
 def create_item(name: str, price: float, og_price: float):
     new_item = Item(name, price, og_price)
+    save_item(new_item)
     return new_item
 
-def save_item(item: Item):
+def save_item(item: Item): # write to csv
+    pass
+
+def delete_item(item: Item):
+    pass
+
+def search_item(key: str):
     pass
 
 def display_items():
     pass
 
-def get_net_prof(profit, shipping, fee_percent):
-    return calc_net_prof(profit, shipping, fee_percent)
+# def get_net_prof(profit, shipping, fee_percent):
+#     return calc_net_prof(profit, shipping, fee_percent)
+
+def get_net_prof_sum(items, shipping, fee_percent): # list of net profits for all items
+    netprofits = []
+    for item in items:
+        profit = item.og_price - item.price
+        netprofits.append(calc_net_prof(profit, shipping, fee_percent))
+    return calc_netprof_sum(netprofits)
 
 def get_price_sum(items: list):
     prices_list = []
--- interface.py
+++ interface.py
@@ -1,10 +1,23 @@
-from application import create_item
+from application import create_item, display_items, delete_item
 
 def interface():
     print("Welcome to SellerTrack")
-    item_name = input("Input name: ")
-    item_price = input("Input price: ")
-    item_og_price = input("Input original price: ")
-    item = create_item(item_name, item_price, item_og_price)
-    print(item)
+    option = input("What would you like to do: ")
+    print("1. Create item\n" \
+          "2. Display item\n" \
+          "3. Delete item\n" \
+          "4. Search for item\n")
+    
+    match option:
+        case 1:
+            item_name = input("Input name: ")
+            item_price = input("Input price: ")
+            item_og_price = input("Input original price: ")
+            create_item(item_name, item_price, item_og_price)
+        case 2:
+            display_items()
+        case 3:
+            delete_item()
+        case 4:
+            pass
 
